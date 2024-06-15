e48a6d747fbcca96a5548838ce4f418935949378
Andrew Ferreira <andrewf289@gmail.com>
2024-06-15 16:01:50 -0400

    calling endpoint and mapping through info works

--- gumroad_landing/client/src/features/courses/CoursesList.jsx
+++ gumroad_landing/client/src/features/courses/CoursesList.jsx
@@ -1,6 +1,45 @@
+import React, {useState, useEffect} from "react";
+
+//import { API_URL } from "../../constants";
+const API_URL = "http://127.0.0.1:3000/api/v1"
+
 function CoursesList() {
+    //Fetch courses somehow
+    const [courses, setCourses] = useState([]);
+    const [,setLoading] = useState(true);
+    const [,setError] = useState(null);
+    //fetch posts from rails API
+    useEffect(() => {
+        async function loadCourses(){
+            console.log(API_URL);
+            try {
+                const response = await fetch(`${API_URL}/courses`);
+                if (response.ok){
+                    const json = await response.json();
+                    setCourses(json);
+                }else {
+                    throw response;
+                }
+            } catch (e) {
+                setError("An Error Occured...");
+                console.log("An error occurred", e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadCourses();
+
+    },[]);
+
     return (
-    <>Courses...</>
+        <div>
+            {courses.map((course) =>(
+                <div key={course.id} className="course-container">
+                    <h1>{course.title}</h1>
+                    <p>{course.price}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
