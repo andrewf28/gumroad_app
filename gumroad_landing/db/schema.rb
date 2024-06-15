d062af5f76013b19e73a966dd69be92700c2e830
Andrew Ferreira <andrewf289@gmail.com>
2024-06-15 17:55:09 -0400

    let's see what happens 👀👀

--- gumroad_landing/db/schema.rb
+++ gumroad_landing/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_15_182906) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_15_214939) do
   create_table "courses", force: :cascade do |t|
     t.string "courseImage"
     t.string "authorAvi"
